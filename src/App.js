304cbe0fda6a06b4c331aa2411cc1788d2644f19
rpvivekp <vvkmrchn@gmail.com>
2022-04-13 21:43:47 +0530

    Project added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+function App(props) {
+  
+  const bookdet=(
+    <ul>
+      {props.book.map((book)=>
+        <div key={book.id}>
+          <h3>{book.bname}</h3>
+          <h4>{book.price}</h4>
+        </div>
+      )}
+    </ul>
+  )
+  const content=(
+    <ul>
+      {props.blog.map((blog)=>
+        <div key={blog.id}>
+          <h1>{blog.blogName}</h1>
+          <h3>{blog.pname}</h3>
+          <h4>{blog.para}</h4>
+        </div>
+      )}
+    </ul>
+  )
+
+
+  const coursedet=(
+    <ul>
+      {props.det.map((detail)=>
+        <div key={detail.id}>
+          <h1>{detail.name}</h1>
+          <h4>{detail.date}</h4>
+        </div>
+      )}
+    </ul> 
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <table className="table">
+    <tr>
+      <div>
+      <td>
+      <div className="mystyle1">
+          <h1 className="tableheader
+">Course Details</h1>
+          {coursedet}
+        </div>
+        </td>
+        <td className="tin">
+        <div className="st2">
+          <h1 className="tableheader">Book Details</h1>
+          {bookdet}
+        </div>
+        </td>
+        <td>
+        <div className="v1">
+          <h1 className="tableheader">Blog Details</h1>
+          {content}
+        </div>
+        </td>
+      </div>
+      </tr>
+      </table>
     </div>
   );
 }
---
+++ src/Books.js
@@ -0,0 +1,14 @@
+const books=[
+    {id:101,bname:'Master React',price:670},
+    {id:102,bname:'Deep Dive into Angular 11',price:800},
+    {id:103,bname:'Mongo Essentials',price:450}
+]
+const BlogDetails=[
+    {id:201,blogName:'React Learning',pname:'Stephan Biz',para:'Welcome to Learning React!'},
+    {id:202,blogName:'Installation',pname:'Schewzdenier',para:'You can install React from npm'}
+]
+const CourseDetail=[
+    {id:301,name:'Angular',date:'4/5/2021'},
+    {id:302,name:'React',date:'6/3/2021'}
+]
+export {books,BlogDetails,CourseDetail};
